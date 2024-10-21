da9558cac0519e6779b3c4cc4eadd84b9a95a7a8
brian <yangbri1@msu.edu>
2024-10-20 21:39:46 -0400

    created single-field index on 'class_id' & 'student_id' using Node driver *programmatical way* not on MongoDB Compass

--- server.mjs
+++ server.mjs
@@ -9,6 +9,7 @@ const app = express();
 dotenv.config();
 let PORT = process.env.PORT || 3001;
 
+
 // Aside: db.createCollection("CollectionName");    // --- creating a collection
 /* Aside+: db.runCommand({
 //              collMod: "CollectionName"
@@ -16,7 +17,9 @@ let PORT = process.env.PORT || 3001;
 // ref: https://www.mongodb.com/docs/manual/core/schema-validation/specify-json-schema/?msockid=0196e6e3201a6f5504d5f2d421086e50
 //      https://www.slingacademy.com/article/how-to-create-validation-rules-in-mongodb-with-examples/
 //      https://www.canva.com/design/DAGUBpKEFtY/JiFgr5uUZI7Ijo58Y4J8OA/edit 
+
 // schema ("cookie cutter") for "grades" collection
+// Note: below was how a schema was shown to be created from 319.4 CodeSandBox
 const gradeSchema = {
     // using MongoDB's $jsonSchema operator
     $jsonSchema: {
@@ -45,10 +48,54 @@ const gradeSchema = {
           }
         }
       },
-      // validationLevel: "strict",    // "strict" settings requires documents to have "class_id" & "student_id" fields that strictly follows respective validation rules
-      validationAction: "warn"         // change validation action to "warn" as instructed
-
+      // validationLevel: "strict",    /* "strict" settings requires documents to have "class_id" & "student_id" fields that strictly follows respective validation rules
+                                       // other options: "off" or "moderate" for schema validation level */
+      validationAction: "warn"         /* change schema validation action to "warn" from default "error"
+                                        "error" - if documents violate validation rules, MongoDB rejects any insert or update */
 };
+
+// below format used to create collection "grades" w/ given validation rules & schema validation action in MongoDB Compass
+// db.createCollection("grades",{
+//     validator: {
+//       $jsonSchema: {
+//         bsonType: "object",
+//         title: "Grades Object Validation",
+//         // list required fields
+//         required: ["class_id", "student_id", "name"],
+//         properties: {
+//           class_id: {
+//             bsonType: "int",
+//             minimum: 0,
+//             maximum: 300,
+//             description: "'class_id' must be an integer b/t 0-300 inclusive"
+//           },
+//           student_id: {
+//             bsonType: "int",
+//             minimum: 0,
+//             description: "'student_id' must be an integer at least 0 or greater"
+//           }
+//         }
+        
+//       }
+//     },
+//     validationAction: "warn"
+// });
+
+
+/* create index using Node driver (.createIndex method) */
+/* create a **single-field** index on "class_id"  */
+// produce a single key ascending index for "class_id" on gradeSchema
+gradeSchema.createIndex({ class_id : 1 });      // format in notes
+
+/* create a **single-field** index on "student_id" */
+// produce a single key descending index for "student_id" on gradeSchema
+gradeSchema.createIndex({ student: -1 });       // format in notes
+
+// alternative formats of above -- create **single-field** indices for "class_id" & "student_id"
+/* under the assumption there's a collection "gradeSchema" w/ documents featuring 
+"class_id" fields we frequently query s.t. we can index it in **ascending** order */
+// db.gradeSchema.createIndex({ class_id: 1 }); // index in ascending order
+// db.gradeSchema.createIndex({ student: -1});  // index in descending order
 
 // middlware
 app.use(bodyParser.urlencoded({extended: true}));
